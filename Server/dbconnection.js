0aa65bc2842f0aa149c8ddd6b772bce486ec1db1
DESKTOP-FKPC2V6\51rem <51remy@live.fr>
2018-11-02 20:42:15 +0100

    correct prod/dev

--- Server/dbconnection.js
+++ Server/dbconnection.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createPool({
 	host: 'localhost',
 	user: 'root',
-	password: '12345678',
+	password: '',
 	database: 'garage',
 });
 module.exports = connection;
